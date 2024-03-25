b10078c15c742c7cc48d26a5ce76de84b74cc1bc
Uma25 <developer333@gmail.com>
2024-03-25 13:26:59 +0300

    Cleaning code and add log

--- src/main/java/com/example/VacationPayCalculator/VacationPayCalculatorApplication.java
+++ src/main/java/com/example/VacationPayCalculator/VacationPayCalculatorApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NeoflexApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NeoflexApplication.class, args);
 	}
